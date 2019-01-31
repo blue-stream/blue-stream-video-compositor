87268da0aa9a1f6aad12c6aa2d4ff6fa7508355b
Almog Vagman Ciprut <almogvc@gmail.com>
2019-01-31 12:12:54 +0200

    fix: Add restream to fix video's create request

--- src/proxies.ts
+++ src/proxies.ts
@@ -4,7 +4,18 @@ import { config } from './config';
 const videosApi: string = `${config.endpoints.videos.hostname}:${config.endpoints.videos.port}`;
 const channelsApi: string = `${config.endpoints.channels.hostname}:${config.endpoints.channels.port}`;
 
-const VideosProxy = proxy({ target: videosApi });
+const restream = (proxyReq: any, req: any, res: any) => {
+    if (req.body) {
+        const bodyData = JSON.stringify(req.body);
+        // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+        proxyReq.setHeader('Content-Type', 'application/json');
+        proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+        // stream the content
+        proxyReq.write(bodyData);
+    }
+};
+
+const VideosProxy = proxy({ target: videosApi, onProxyReq: restream });
 const ChannelsProxy = proxy({ target: channelsApi });
 
 export { VideosProxy, ChannelsProxy };

24204a54608ee2c818a9acebb93bc6e98ee47925
Almog Vagman Ciprut <almogvc@gmail.com>
2019-03-28 10:46:16 +0200

    fix: fix APM_ACTIVE flag

--- src/config.ts
+++ src/config.ts
@@ -68,7 +68,7 @@ export const config = {
     },
     apm: {
         server: process.env.APM_SERVER || 'http://apm:8200',
-        isActive: process.env.APM_ACTIVE || true,
+        isActive: +(process.env.APM_ACTIVE || 1),
     },
     videoSecret: 'video@bs2019!',
 };

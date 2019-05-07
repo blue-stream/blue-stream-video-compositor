efa15d30aaca8c0076bce846017663c45ad14ff4
Almog Vagman Ciprut <almogvc@gmail.com>
2019-05-07 16:23:57 +0300

    feat: add views to channel search

--- src/channels/channels.service.ts
+++ src/channels/channels.service.ts
@@ -4,12 +4,16 @@ import { config } from '../config';
 export class ChannelsService {
     static api: string = `${config.endpoints.channels.hostname}:${config.endpoints.channels.port}${config.endpoints.channels.api}`;
 
-    static  get(channelId: string, authorizationHeader: string) {
+    static get(channelId: string, authorizationHeader: string) {
         return HttpClient.get(`${ChannelsService.api}/${channelId}`, null, authorizationHeader);
     }
 
-    static  getMany(query: any, authorizationHeader: string) {
+    static getMany(query: any, authorizationHeader: string) {
         return HttpClient.get(`${ChannelsService.api}/many`, query, authorizationHeader);
+    }
+
+    static getSearched(query: any, authorizationHeader: string) {
+        return HttpClient.get(`${ChannelsService.api}/search`, query, authorizationHeader);
     }
 
     static async doesExist(channelId: string, authorizationHeader: string) {
